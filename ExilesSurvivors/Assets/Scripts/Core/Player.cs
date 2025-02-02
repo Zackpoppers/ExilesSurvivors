a3d0d6b89d47a858dc40857cbac70f72b4560676
magicdracon <ascendium69@gmail.com>
2025-02-02 00:13:03 -0700

    Optimized performace for supports
    Optimized performace for supports, didnt realize i was applying the support gems multiple times, so instead of projectiles forking once, they were forking 50x times.

--- ExilesSurvivors/Assets/Scripts/Core/Player.cs
+++ ExilesSurvivors/Assets/Scripts/Core/Player.cs
@@ -8,7 +8,12 @@ public class Player : MonoBehaviour
     public float Armor = 10f;
     public float MovementSpeed = 5f;
     public List<Skill> ActiveSkills = new List<Skill>();
-    public Inventory Inventory; 
+    public Inventory Inventory;
+
+    private void Start()
+    {
+        ActiveSkills[0].SupportGems[0].ApplySupport(ActiveSkills[0]);
+    }
 
     private void Update()
     {
@@ -27,7 +32,9 @@ public class Player : MonoBehaviour
     {
         if (Input.GetKeyDown(KeyCode.Alpha1) && ActiveSkills.Count > 0)
         {
-            ActiveSkills[0].SupportGems[0].ApplySupport(ActiveSkills[0]);
+            
+            //ActiveSkills[0].SupportGems[1].ApplySupport(ActiveSkills[0]);
+
             ActiveSkills[0].Activate(this);
             
         }
